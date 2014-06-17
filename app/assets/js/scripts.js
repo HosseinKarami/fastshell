c5afe815958160771a078bdce424486a1843e91d
Hossein Karami <athreepik@gmail.com>
2014-06-18 00:54:55 +0430

    Add Banner to Css and Js, Update Dependencies

--- app/assets/js/scripts.js
+++ app/assets/js/scripts.js
@@ -1,3 +1,11 @@
+/*!
+ * fastshell
+ * Fiercely quick and opinionated front-ends
+ * http://fastshell.com
+ * @author Hossein Karami
+ * @version 1.0.1
+ * Copyright 2014. MIT licensed.
+ */
 (function ($, window, document, undefined) {
 
   'use strict';
--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,22 @@ var gulp = require('gulp'),
     autoprefixer = require('gulp-autoprefixer'),
     uglify = require('gulp-uglify'),
     jshint = require('gulp-jshint'),
-    rename = require('gulp-rename');
+    header  = require('gulp-header'),
+    rename = require('gulp-rename'),
+    package = require('./package.json');
 
+
+var banner = [
+  '/*!\n' +
+  ' * <%= package.name %>\n' +
+  ' * <%= package.title %>\n' +
+  ' * <%= package.url %>\n' +
+  ' * @author <%= package.author %>\n' +
+  ' * @version <%= package.version %>\n' +
+  ' * Copyright ' + new Date().getFullYear() + '. <%= package.license %> licensed.\n' +
+  ' */',
+  '\n'
+].join('');
 
 gulp.task('css', function () {
     return gulp.src('src/scss/style.scss')
@@ -14,28 +28,37 @@ gulp.task('css', function () {
     .pipe(gulp.dest('app/assets/css'))
     .pipe(sass({ style: 'compressed' }))
     .pipe(rename({ suffix: '.min' }))
+    .pipe(header(banner, { package : package }))
     .pipe(gulp.dest('app/assets/css'))
+    .pipe(browserSync.reload({stream:true}));
 });
 
 gulp.task('js',function(){
   gulp.src('src/js/scripts.js')
     .pipe(jshint('.jshintrc'))
     .pipe(jshint.reporter('default'))
+    .pipe(header(banner, { package : package }))
     .pipe(gulp.dest('app/assets/js'))
     .pipe(uglify())
+    .pipe(header(banner, { package : package }))
     .pipe(rename({ suffix: '.min' }))
     .pipe(gulp.dest('app/assets/js'))
-})
+    .pipe(browserSync.reload({stream:true, once: true}));
+});
 
 gulp.task('browser-sync', function() {
-    browserSync.init(["app/assets/css/*.css", "app/**/*.html", "app/assets/js/**/*.js"], {
+    browserSync.init(null, {
         server: {
             baseDir: "app"
         }
     });
 });
+gulp.task('bs-reload', function () {
+    browserSync.reload();
+});
 
 gulp.task('default', ['css', 'js', 'browser-sync'], function () {
     gulp.watch("src/scss/*/*.scss", ['css']);
     gulp.watch("src/js/*.js", ['js']);
+    gulp.watch("*.html", ['bs-reload']);
 });
