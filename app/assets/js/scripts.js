6f971755de5cb636a1e5d7d28a9ba93f0213d6ab
Hossein Karami <athreepik@gmail.com>
2015-12-14 19:51:04 +0330

    V1.0.5

--- app/assets/js/scripts.js
+++ app/assets/js/scripts.js
@@ -3,8 +3,8 @@
  * Fiercely quick and opinionated front-ends
  * https://HosseinKarami.github.io/fastshell
  * @author Hossein Karami
- * @version 1.0.0
- * Copyright 2014. MIT licensed.
+ * @version 1.0.5
+ * Copyright 2015. MIT licensed.
  */
 (function (window, document, undefined) {
 
